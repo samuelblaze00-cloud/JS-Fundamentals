636b000fdacb5585d96aa7ce06e2d41bc15938cd
samuel <samuelblaze00@gmail.com>
2025-09-12 11:54:34 +0100

    Added

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,4 @@
-const names = ["C is fun", "Python is cool", "Javascript is amazing"];
+const names = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for (let i = 0; i < names.length; i++) {
     console.log(names[i]);
 }
